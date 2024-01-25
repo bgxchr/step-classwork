a43a44123b8bff768ef56ed96ea06f3ec234d5e9
Donica Mihaela <donica.mihaela07@gmail.com>
2024-01-25 19:34:46 +0200

    Classwork-if

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+ public class HelloWorld {
+         public static void main(String[] args) {
+         }
+         int u = 23;
+
+        if(u %2==0)
+
+         {
+                 System.out.println("Variabila int este pară.");
+         } else
+
+         {
+                 System.out.println("Variabila int este impară.");
+
+         }
+ }
